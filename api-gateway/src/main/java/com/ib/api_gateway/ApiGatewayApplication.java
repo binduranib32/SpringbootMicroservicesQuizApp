433b04a2be97533e34e954e91880de061d0a8838
iambindu <binduranib32@gmail.com>
2025-03-05 01:45:40 +0530

    Initial commit: Added 4 microservices

---
+++ api-gateway/src/main/java/com/ib/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ib.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ quiz-service/src/main/java/com/ib/quiz_service/controller/QuizController.java
@@ -0,0 +1,37 @@
+package com.ib.quiz_service.controller;
+
+import com.ib.quiz_service.model.QuestionWrapper;
+import com.ib.quiz_service.model.QuizDto;
+import com.ib.quiz_service.model.Response;
+import com.ib.quiz_service.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    //public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
+    public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto){
+        return quizService.createQuiz(quizDto.getCategoryName(), quizDto.getNumQuestions(), quizDto.getTitle());
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+}
